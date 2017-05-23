0780b06b150116543025f5534b35ed5609ff22f2
Andres Calfulaf <andrescalfulaf@MBP-de-Andres.home>
2017-05-23 16:35:37 -0400

    Ajustes al listado de los niños

--- src/pages/lista/lista.ts
+++ src/pages/lista/lista.ts
@@ -12,19 +12,48 @@ import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
   selector: 'page-lista',
   templateUrl: 'lista.html',
 })
+
 export class Lista {
-  ninos: string = "no";
+  ninos:Array<nino>=[{
+    img:"assets/img/David.png",
+    nombre:"Axel Calfulaf B",
+    ap1:"Andres Calfulaf P",
+    fono1:"942693087",
+    estado:"false"
+  },
+  {
+    img:"assets/img/Ester.png",
+    nombre:"Amy Calfulaf O",
+    ap1:"Andres Calfulaf P",
+    ap2:"Jacqueline Obando D",
+    fono1:"942693087",
+    fono2:"942693087",
+    estado:"false"
+  }];
+   ninosx: string = "false";
   isAndroid: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,platform: Platform) {
     this.isAndroid = platform.is('android');
   }
-  
 
-  
-
+  cambiarEstado(n:any):void{
+    if(n.estado=="true")
+      n.estado="false";
+    else 
+      n.estado="true";
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad Lista');
   }
 
+}
+interface nino{
+  img:string;
+  nombre:string;
+  ap1:string;
+  ap2?:string;
+  fono1:string;
+  fono2?:string;
+  estado:string;
 }
